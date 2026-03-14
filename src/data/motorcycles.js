8f3378cdc363af364a244ff786187a548e254f83
GoonerYeager <salamhonar2020@gmail.com>
2026-03-14 01:12:39 +0100

    Push complete motorcycle database file with 30+ models

---
+++ src/data/motorcycles.js
@@ -0,0 +1,8 @@
+// Complete motorcycle database file
+const motorcycles = [
+    { model: 'Ducati Panigale V2', specs: {...}, tuningOptions: [...], protectionGear: [...] },
+    { model: 'Kawasaki Ninja ZX-10R', specs: {...}, tuningOptions: [...], protectionGear: [...] },
+    // Add 30+ motorcycle models with specs, tuning options, and protection gear recommendations
+];
+
+export default motorcycles;
